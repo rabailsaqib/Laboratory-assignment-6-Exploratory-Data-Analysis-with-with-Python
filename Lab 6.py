158b924fdf410f8018f2df9234b3b4334d7ff61b
eva-sorge <eva.sorge9@gmail.com>
2025-10-21 09:02:19 -0400

    #3

--- Lab 6.py
+++ Lab 6.py
@@ -6,7 +6,35 @@ Created on Tue Oct 21 08:16:36 2025
 @author: evasorge
 """
 
+
 #Questions to answer:
     #1. Does internet usage affect greenhouse gas emissions?
     #2. How is GNI affected by education?
     #3. What regions are the richest?
+    
+    
+
+#Load dataset using pandas
+
+import pandas as pd
+import seaborn as sns
+data = pd.read_csv('wdi_wide.csv')
+
+#Accessing wanted data
+
+GHG = data["Greenhouse gas emissions"]
+internet_usage = data["Internet use"]
+GNI = data["GNI"]
+male_education = data["Tertiary education, male"]
+female_education = data["Tertiary education, female"]
+regions = data["Region"]
+
+#Question 3:
+    
+data.info()
+data.isnull().sum()
+
+
+
+
+
