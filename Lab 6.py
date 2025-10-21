edc1eb39ec7a7eb392a6b29b410c2f468c18bece
eva-sorge <eva.sorge9@gmail.com>
2025-10-21 08:33:08 -0400

    questions

--- Lab 6.py
+++ Lab 6.py
@@ -6,3 +6,7 @@ Created on Tue Oct 21 08:16:36 2025
 @author: evasorge
 """
 
+#Questions to answer:
+    #1. Does internet usage affect greenhouse gas emissions?
+    #2. How is GNI affected by education?
+    #3. What regions are the richest?
